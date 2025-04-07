19b7231fd20a3cdd5b6f9ac2f49460120868f3c8
JuanDiego <juand.lopezm@autonoma.edu.co>
2025-04-06 20:42:23 -0500

    Se agrergaron nuevos endopoints para administrador, asi darle una gestion de los envios como un administrador

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,15 +1,21 @@
 import express from 'express';
-import { getUsers, getUser, createUser, updateUser, deleteUser, searchUsers, getGeneralStats } from '../controllers/adminController.js';
+import { getUsers, getUser, createUser, updateUser, deleteUser, searchUsers, getShipments, searchShipment, getGeneralStats } from '../controllers/adminController.js';
 import { validateUser, validateUserUpdate } from '../validators/userValidator.js';
+import { validateShipmentUpdate } from '../validators/shipmentValidator.js';
 import validateRequest from '../middlewares/validateRequest.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import roleMiddleware from '../middlewares/roleMiddleware.js';
+import { deleteShipment, updateShipment } from '../controllers/shipmentController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, roleMiddleware(['admin']), getUsers);   // Obtener todos los usuarios (Admin)
 
+router.get('/shipments', authMiddleware, roleMiddleware(['admin']), getShipments);   // Obtener todos los envios (Admin)
+
 router.get('/search', authMiddleware, roleMiddleware(['admin']), searchUsers);
+
+router.get('/shipments/search', authMiddleware, roleMiddleware(['admin']), searchShipment);
 
 router.get('/stats', authMiddleware, roleMiddleware(['admin']), getGeneralStats); // Obtener estadísticas generales (Admin)
 
@@ -19,6 +25,12 @@ router.post('/', authMiddleware, roleMiddleware(['admin']), validateRequest(vali
 
 router.put('/:id', authMiddleware, validateRequest(validateUserUpdate), roleMiddleware(['admin']), updateUser);     // Actualizar un usuario (Admin)
 
+router.put('/shipments/:id', authMiddleware, roleMiddleware(['admin']),validateRequest(validateShipmentUpdate), updateShipment);     // Actualizar un envio (Admin)
+
 router.delete('/:id', authMiddleware, roleMiddleware(['admin']), deleteUser);    // Borrar un usuario (Admin)
+
+router.delete('/shipments/:id', authMiddleware, roleMiddleware(['admin']), deleteShipment); // Borrar un envio
+
+
 
 export default router;
